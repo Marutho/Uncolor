1a3c971dfc52f8f65953ffb6204e4c79bb13e787
UnityTrash <alu.74991@usj.es>
2020-06-25 20:48:35 +0200

    first commit

---
+++ Assets/Samples/Yarn Spinner/1.2.6/Space/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Yarn.Unity.Example {
+    /// attached to the non-player characters, and stores the name of the Yarn
+    /// node that should be run when you talk to them.
+
+    public class NPC : MonoBehaviour {
+
+        public string characterName = "";
+
+        public string talkToNode = "";
+
+        [Header("Optional")]
+        public YarnProgram scriptToLoad;
+
+        void Start () {
+            if (scriptToLoad != null) {
+                DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+                dialogueRunner.Add(scriptToLoad);                
+            }
+        }
+    }
+
+}
